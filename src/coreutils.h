f1d3799618e686249bf486ae1e8be9b612f2c1da
Dorif <dorif11@gmail.com>
2013-01-07 21:16:02 +0200

    coreutils.h reworked

--- src/coreutils.h
+++ src/coreutils.h
@@ -9,9 +9,3 @@ void usage(char *hs){
 	write(STDOUT_FILENO,hs,strlen(hs));
 	_exit(0);
 }
-char* itoa(unsigned int val, short base){
-	static char buf[11];
-	short i=10;
-	for(;val&&i;--i,val/=base)buf[i]=val%base+48;
-	return &buf[i+1];
-}
