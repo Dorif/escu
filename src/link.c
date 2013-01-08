0d989c2cd4fb7c01bd4f67a958b8eb78972f1c6c
Dorif <dorif11@gmail.com>
2013-01-08 16:41:16 +0200

    code optimization

--- src/link.c
+++ src/link.c
@@ -3,6 +3,6 @@
 char help_str[]="Usage: link file1 file2\n";
 int main(int argc, char** argv){
 	if(argc>3)usage(help_str);
-	if(link(argv[1], argv[2]) == -1)ferr();
+	if(link(argv[1], argv[2]))ferr();
 	_exit(0);
  }
